bd4bcb0cda46b47d14389d83ca0723e9d1baf21a
Yetunde Animashawun <yanim87@gmail.com>
2023-03-27 19:23:20 +0100

    products, price, image added

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Header from './Header.js';
+import Home from './Home.js';
+import Product from './Product.js';
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(<App />);
+root.render(<Header />);
+ root.render(<Home />);
+ root.render(<Product />);
 
