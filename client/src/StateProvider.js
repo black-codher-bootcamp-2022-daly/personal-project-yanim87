fa71de9541eb0703a09d78424910e515696c09f9
Yetunde Animashawun <yanim87@gmail.com>
2023-03-31 07:52:39 +0100

    added reducer and statprovider for data layer to add basket

---
+++ client/src/StateProvider.js
@@ -0,0 +1,11 @@
+import React, {createContext, useContext, useReducer} from "react";
+// prepares the data layer
+export const StateContext = createContext();
+//wraps our app and provide data layer to every component
+export const StateProvider = ({reducer, initialState, children}) => {
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+    </StateContext.Provider>
+};
+// pull information from data layer
+export const useStateValue = () => useContext(StateContext);
